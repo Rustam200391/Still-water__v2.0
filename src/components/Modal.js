07033a30d84dd3b29a119a0ff1c7d96fb493b94f
Rustam Guseinov <rg20031991.com>
2023-08-06 21:16:57 +0300

    add sass commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
-import style from "./style.module.css";
+import style from "./style.module.scss";
 
 // контеннт модального окна
 
@@ -14,59 +14,32 @@ const customModalContent = (
 const ModalComponent = ({ isOpen, onClose }) => (
   <Modal isOpen={isOpen} onRequestClose={onClose} className={style.modal}>
     {customModalContent}
-
-    <form className={style.block}>
-      <div className={style.block__information}>
-        <div className={style.block__title__place}>
-          <label for="name-sources">Наименование</label>
-          <input
-            type="text"
-            name="name-sources"
-            className={style.block__title}
-          />
-        </div>
-        <div className={style.block__title__place}>
-          <label htmlFor="name-sources">Местоположение</label>
-
-          <input
-            type="text"
-            name="name-sources"
-            id="cities"
-            className={style.block__title}
-          />
-        </div>
-
-        <div className={style.add__block__description}>
-          <label htmlFor="input">Описание</label>
-          <textarea id="story" name="story" rows="5" cols="13" />
-        </div>
-        <div className={style.block__title__photo}>
-          <label name="Upload an Image" className={style.add__block}>
-            Фото источника
-          </label>
-          <input
-            type="file"
-            //переименовать кнопку
-            id="file"
-            name="Upload an Image"
-            placeholder="Upload an Image"
-            required
-            className={style.block__upload}
-          />
-          <input type="submit" value="загрузить" id="submit" />
-        </div>
+    <form className={style.form}>
+      <div className="question">
+        <input className={style.input} type="text" required />
+        <label className={style.label} htmlFor="name">
+          Название
+        </label>
       </div>
-      <div>
-        <button className={style.close__button} onClick={onClose}>
-          <img
-            src="https://www.magicwaters.ru/webp/upload/dynamic/2017-06/05/pk-empty.png.webp"
-            height={58}
-            width={26}
-            alt=""
-          />
-        </button>
+      <div class="question">
+        <input className={style.input} type="text" required />
+        <label className={style.label}>Местоположение</label>
       </div>
+
+      <div class="question">
+        <textarea required />
+        <label className={style.label}>Добавить описание</label>
+      </div>
+      <button className={style.button}>Добавить</button>
     </form>
+    <button className={style.close__button} onClick={onClose}>
+      <img
+        src="https://www.magicwaters.ru/webp/upload/dynamic/2017-06/05/pk-empty.png.webp"
+        height={58}
+        width={26}
+        alt=""
+      />
+    </button>
   </Modal>
 );
 

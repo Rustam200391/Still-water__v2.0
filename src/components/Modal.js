03a6fbb8f5c1ba28f7864a63023fc8e901751657
Rustam Guseinov <rg20031991.com>
2023-08-08 18:29:45 +0300

    fix the bag in modal commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,37 +1,49 @@
 import React from "react";
 import Modal from "react-modal";
+import axios from "axios";
 import style from "./style.module.scss";
 
 // контеннт модального окна
 
 const customModalContent = <div className={style.close__title}></div>;
 
+const onSubmit = async (data) => {
+  const { name, place, description, file } = data;
+  const url = "http://localhost:8000/api/v1/users/";
+  try {
+    await axios
+      .post(url, {
+        name: name,
+        place: place,
+        description: description,
+        file: file,
+      })
+      .then((res) => {
+        if (res.statusCode === 201) {
+          navigate("/home");
+        }
+      })
+      .catch((err) => {
+        console.log("ошибка");
+      });
+  } catch (err) {
+    console.log("ошибка", err);
+  }
+};
 // компонент модального окна
 const ModalComponent = ({ isOpen, onClose }) => (
-  <Modal isOpen={isOpen} onRequestClose={onClose} className={style.modal}>
+  <Modal
+    isOpen={isOpen}
+    onRequestClose={onClose}
+    ariaHideApp={false}
+    className={style.modal}
+  >
     {customModalContent}
-    {/* <form className={style.form}>
-      <div className="question">
-        <input className={style.input} type="text" required />
-        <label className={style.label} htmlFor="name">
-          Название
-        </label>
-      </div>
-      <div class="question">
-        <input className={style.input} type="text" required />
-        <label className={style.label}>Местоположение</label>
-      </div>
 
-      <div class="question">
-        <textarea required />
-        <label className={style.label}>Добавить описание</label>
-      </div>
-      <button className={style.button}>Добавить</button>
-    </form> */}
     <form className={style.form}>
       <h1 className={style.h1}>Информация об источнике</h1>
       <div className={style.form__input}>
-        <label className={style.label} for="name">
+        <label className={style.label} htmlFor="name">
           Наименование:
         </label>
         <input
@@ -43,26 +55,26 @@ const ModalComponent = ({ isOpen, onClose }) => (
         />
       </div>
       <div className={style.form__input}>
-        <label className={style.label} for="email">
+        <label className={style.label} htmlFor="place">
           Местоположение:
         </label>
         <input type="text" id={style.place} name="place" required />
       </div>
       <div className={style.form__input}>
-        <label className={style.label} for="description">
+        <label className={style.label} htmlFor="description">
           Описание:
         </label>
         <textarea id={style.description} name="description" required />
       </div>
       <div className={style.form__input}>
-        <label className={style.label} for="photo">
+        <label className={style.label} htmlFor="photo">
           Фото:
         </label>
         <input
           type="file"
           capture="user" //environment
           name="photo"
-          // accept="image/*"
+          accept="image/*"
           required
         />
       </div>
